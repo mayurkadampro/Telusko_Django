5153bd2274d81b271ebcc6a83bf32a35b3e7c367
Mayur Kadam <mayurkadam200@gmail.com>
2019-05-20 19:18:08 +0530

    Passing Dynamic Data in Html - 2

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -3,9 +3,31 @@ from . models import Destination
 
 # Create your views here.
 def index(request):
-    dest = Destination()
-    dest.name = 'Mumbai'
-    dest.description = 'City Of Gold'
-    dest.price = 700
 
-    return render(request,"index.html",{'destt': dest})
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.description = 'City Of Gold'
+    dest1.price = 700
+    dest1.image = "destination_1.jpg"
+
+    dest2 = Destination()
+    dest2.name = 'Goa'
+    dest2.description = 'Enjoy Ur Holidays'
+    dest2.price = 900
+    dest2.image = "destination_2.jpg"
+
+    dest3 = Destination()
+    dest3.name = 'Bangalore'
+    dest3.description = 'A place which teaches us to chill.'
+    dest3.price = 800
+    dest3.image = "destination_3.jpg"
+
+    dest4 = Destination()
+    dest4.name = 'singapore'
+    dest4.description = 'Passion Made Possible'
+    dest4.price = 1000
+    dest4.image = "destination_4.jpg"
+
+    distt = [dest1,dest2,dest3,dest4]
+
+    return render(request,"index.html",{'destt': distt})
