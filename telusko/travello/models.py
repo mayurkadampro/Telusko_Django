f6096de30c675b67c915c073df806cc1d7e34bd4
Mayur Kadam <mayurkadam200@gmail.com>
2019-05-24 19:41:57 +0530

    Models and Migrations

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id : int
-    name : str
-    description : str
-    image: str
-    price : int
-    offer : bool
+class Destination(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    imagev = models.ImageField(upload_to = 'pics')
+    price = models.IntegerField()
+    offer = models.BooleanField(default = False)
 
